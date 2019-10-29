ad6a42cfd2c412ea0c4e2f4594fa1b67401e1a34
jonathantang <jonathantang0126@gmail.com>
2019-10-30 00:27:34 +0800

    create feature of user sign up

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const restController = require('../controllers/restController.js')
+const userController = require('../controllers/userController')
 
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
 
   app.get('/restaurants', restController.getRestaurants)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }

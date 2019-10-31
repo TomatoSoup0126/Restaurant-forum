66ff5b1b4a2c81f6519ded1d3cd09f9b34998678
jonathantang <jonathantang0126@gmail.com>
2019-10-31 09:08:20 +0800

    add login function by Passport

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const restController = require('../controllers/restController.js')
 const userController = require('../controllers/userController')
 
-module.exports = app => {
+module.exports = (app, passport) => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
 
   app.get('/restaurants', restController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
+
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  app.get('/logout', userController.logout)
 }

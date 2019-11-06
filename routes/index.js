b52984898d883984464f145e51ff68b7def524b1
jonathantang <jonathantang0126@gmail.com>
2019-11-06 23:48:40 +0800

    Add restaurants show page

--- routes/index.js
+++ routes/index.js
@@ -43,6 +43,8 @@ module.exports = (app, passport) => {
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
   app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
+  app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+
 
 
 

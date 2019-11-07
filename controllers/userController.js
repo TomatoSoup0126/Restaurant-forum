533492d61f9beb95f5887f6ce70ac036abc87cd2
jonathantang <jonathantang0126@gmail.com>
2019-11-07 16:31:05 +0800

    add user profile Read/Update feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
 
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = '139178d64d36915'
+
 const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
@@ -43,6 +46,64 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  getUser: (req, res) => {
+    return User.findByPk(req.params.id)
+      .then(userResult => {
+        return res.render('user', { userResult })
+      })
+  },
+
+  editUser: (req, res) => {
+    let user = req.user
+    return User.findByPk(req.params.id)
+      .then(userResult => {
+        if (userResult.id !== user.id) {
+          req.flash('error_messages', '嗶嗶! 不可以改人家的資訊!')
+          res.redirect(`/users/${req.params.id}`)
+        } else {
+          return res.render('editUser', { userResult })
+        }
+      })
+  },
+
+  putUser: (req, res) => {
+    console.warn(req)
+    if (!req.body.name) {
+      req.flash('error_messages', "記得填名字!")
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : restaurant.image,
+            })
+              .then((user) => {
+                req.flash('success_messages', '個人資訊更新囉')
+                res.redirect(`/users/${req.params.id}`)
+              })
+          })
+      })
+    }
+    else
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name,
+            image: user.image,
+          })
+            .then((user) => {
+              req.flash('success_messages', '個人資訊更新囉')
+              res.redirect(`/users/${req.params.id}`)
+            })
+        })
   }
 }
 

019fce7cf9a3aca6bb2e77aeaa86018dcef0c848
jonathantang <jonathantang0126@gmail.com>
2019-11-13 00:00:42 +0800

    refactor adminController.getRestaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,10 +8,11 @@ const Category = db.Category
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = '139178d64d36915'
 
+const adminService = require('../services/adminService.js')
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ include: [Category] }).then(restaurants => {
-      return res.render('admin/restaurants', { restaurants })
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants', data)
     })
   },
 

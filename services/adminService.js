69c2463be767eb26143e09531e6ede1126eec8a1
jonathantang <jonathantang0126@gmail.com>
2019-11-16 13:25:54 +0800

    refactor adminController.deleteRestaurant

--- services/adminService.js
+++ services/adminService.js
@@ -17,6 +17,16 @@ const adminService = {
     return Restaurant.findByPk(req.params.id, { include: Category }).then(restaurant => {
       callback({ restaurant: restaurant })
     })
+  },
+
+  deleteRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id)
+      .then((restaurant) => {
+        restaurant.destroy()
+          .then((restaurant) => {
+            callback({ status: 'success', message: '' })
+          })
+      })
   }
 }
 
